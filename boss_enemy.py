edb7100d5859703534a10c7865cf48404a9ed14e
Zayeem Zaman <zayeem@vt.edu>
2021-08-30 23:10:25 -0400

    Basic weakness reveal battle system implemented

--- boss_enemy.py
+++ boss_enemy.py
@@ -14,17 +14,31 @@ from elements import GuardElement
 from random import shuffle
 from random import randint
 
+VULN_MULT = 4
+WEAK_MULT = 2
+NORM_MULT = 1
+RES_MULT = 0.5
+RPL_MULT = -1
+
 class Boss():
     def __init__(self):
         self.damageCounter = 0
-        self.weaknesses = [3, 2, 2, 2, 1, 1, 1, 1, 0.5, 0.5, 0.5, -1]
+        self.weaknesses = [VULN_MULT, WEAK_MULT, WEAK_MULT, WEAK_MULT, NORM_MULT, NORM_MULT, NORM_MULT, NORM_MULT, RES_MULT, RES_MULT, RES_MULT, RPL_MULT]
         self.weakReveal = [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0]
         self.attackType = GuardElement.PHYSICAL
 
         shuffle(self.weaknesses)
 
-    def takeDamage(self, damage):
-        self.damageCounter = self.damageCounter + damage
+    def takeDamage(self, damage, element):
+        calculatedDamage = (damage * self.weaknesses[element]) if (self.weaknesses[element] != RPL_MULT) else 0
+        if calculatedDamage != 0:
+            calculatedDamage = calculatedDamage + randint(-150, 350)
+            self.damageCounter = self.damageCounter + calculatedDamage
+        self.weakReveal[element] = 1
+
+        damage_dict = { VULN_MULT : "Vulnerable! ", WEAK_MULT : "Weak! ", NORM_MULT : "", RES_MULT : "Resisted! " , RPL_MULT : "Repelled! "}
+
+        return damage_dict[self.weaknesses[element]], calculatedDamage
 
     def determineAttackType(self):
         self.attackType = randint(GuardElement.PHYSICAL, GuardElement.MAGICAL)
@@ -32,12 +46,17 @@ class Boss():
     def attack(self):
         return self.attackType
 
+    def shuffleWeaknesses(self):
+        shuffle(self.weaknesses)
+
+        self.weakReveal = [0 for i in self.weakReveal]
+
     def displayWeaknesses(self):
         values = ""
         text = ["SWD", "LNC", "AXE", "BOW", "DAG", "STA", "FIR", "WAT", "WND", "EAR", "LIG", "SHD"]
         display = ""
 
-        weakness_dict = {3 : "VUL", 2 : "WEA", 1 : "NOR", 0.5 : "RES", -1 : "RPL"}
+        weakness_dict = {VULN_MULT : "VUL", WEAK_MULT : "WEA", NORM_MULT : "NOR", RES_MULT : "RES", RPL_MULT : "RPL"}
 
         for type in DamageElement:
             values = values + (weakness_dict[self.weaknesses[type]] if self.weakReveal[type] == 1 else " ? ") + " "
--- dot_bs_main.py
+++ dot_bs_main.py
@@ -8,16 +8,40 @@ The main file for the Default Octopath Tensei Battle System. Run this to play th
 
 from random import randint
 from elements import DamageElement
-from boss_enemy import Boss
+from boss_enemy import NORM_MULT, RES_MULT, RPL_MULT, VULN_MULT, WEAK_MULT, Boss
 
 if __name__ == '__main__':
-    test = ["SWD", "LNC", "AXE", "BOW", "DAG", "STA", "FIR", "WAT", "WND", "EAR", "LIG", "SHD"]
+    attack_dict = {"sword" : DamageElement.SWORD, "lance" : DamageElement.LANCE, "axe" : DamageElement.AXE,
+                    "bow" : DamageElement.BOW, "dagger" : DamageElement.DAGGER, "staff" : DamageElement.STAFF,
+                    "fire" : DamageElement.FIRE, "water" : DamageElement.WATER, "wind" : DamageElement.WIND,
+                    "earth" : DamageElement.EARTH, "light" : DamageElement.LIGHT, "shadow" : DamageElement.SHADOW,
+                    "swd" : DamageElement.SWORD, "lnc" : DamageElement.LANCE,
+                    "dag" : DamageElement.DAGGER, "sta" : DamageElement.STAFF,
+                    "fir" : DamageElement.FIRE, "wat" : DamageElement.WATER, "wnd" : DamageElement.WIND,
+                    "ear" : DamageElement.EARTH, "lig" : DamageElement.LIGHT, "shd" : DamageElement.SHADOW}
+
+    turncount = 1
     enemy = Boss()
-    
-    for i in range(0,12):
-        enemy.weakReveal[i] = randint(0,1)
 
-    print(enemy.weakReveal)
-    print(enemy.weaknesses)
-
+    print("Turn: " + str(turncount))
     print(enemy)
+
+    while True:
+        attack = input("Attack: ").lower().strip()
+        attack = attack_dict.get(attack)
+
+        if attack == None:
+            print("Bad input")
+            continue
+        else:
+            status, damage = enemy.takeDamage(1000, attack)
+            print(status + str(damage) + " damage")
+            turncount = turncount + 1
+
+            if turncount % 8 - 1 == 0:
+                enemy.shuffleWeaknesses()
+
+            print("Turn: " + str(turncount))
+            print(enemy)
+
+        
