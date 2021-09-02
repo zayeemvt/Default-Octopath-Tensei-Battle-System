f3c91842c8cf7aba18db7fdbca7df92c35eb1495
Zayeem Zaman <zayeem@vt.edu>
2021-09-02 16:27:37 -0400

    Added help menu/instructions when booting up game

--- dot_bs_main.py
+++ dot_bs_main.py
@@ -9,7 +9,31 @@ The main file for the Default Octopath Tensei Battle System. Run this to play th
 from elements import attack_dict
 from boss_enemy import NORM_MULT, RES_MULT, RPL_MULT, VULN_MULT, WEAK_MULT, Boss
 
+def helpMenu():
+    print("\n===================================================")
+    print("============= DEFAULT OCTOPATH TENSEI =============")
+    print("============== BATTLE SYSTEM LOADED ===============")
+    print("===================================================")
+    print("\n")
+    print("===================================================")
+    print("Deal as much damage to the boss as you can!")
+    print("You have six weapons and six magic spells at your disposal.")
+    print("Figure out which weapon or spell the boss is weak to by attacking the boss.")
+    print("To attack, type in the name of the weapon or spell you want to use.")
+    print("The boss's weaknesses will change after a certain amount of turns.")
+    print("===================================================")
+    print("Weapons - sword, lance, axe, bow, dagger, staff")
+    print("Magic - fire, water, wind, earth, light, shadow")
+    print("===================================================")
+    print("\n===================================================")
+    print("             <<<Press ENTER to start>>>            ")
+    print("===================================================")
+    input()
+    print("\n================== BATTLE START ==================")
+
 if __name__ == '__main__':
+    helpMenu()
+
     turncount = 1
     enemy = Boss()
 
@@ -37,5 +61,3 @@ if __name__ == '__main__':
 
             print("Turn: " + str(turncount))
             print(enemy)
-
-        
